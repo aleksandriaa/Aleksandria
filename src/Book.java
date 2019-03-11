63d98ba7b6ff5fe5071b6b341f94b402e287796e
TomaszSidor <tom.sidor@gmail.com>
2019-03-11 21:28:41 +0100

    first commit

---
+++ src/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+
+    private String title;
+    private String description;
+    private String author;
+    private int pages;
+
+    private enum genre {
+        Fantasy,
+        Westerns,
+        Romance,
+        Thriller,
+        Mystery,
+        Memoir,
+        Biography,
+        Musical,
+        Satire,
+        Haiku,
+        Horror,
+        DoItYorselfe,
+        Dictionary;
+    }
+
+    private int totalAmount;
+    private int howManyAvaible;
+
+
+}
---
+++ src/Vinyl.java
@@ -0,0 +1,19 @@
+public class Vinyl {
+    private String title;
+    private String artist;
+    private enum genre {
+        POP,
+        ROCK,
+        METAL,
+        DISCO_POLO,
+        FOLK,
+        RAP,
+        HOUSE,
+        TECHNO,
+        JAZZ,
+        ELEKTO,
+        REGGAE,
+        BLUES,
+    }
+
+}
