e1e04a2a7c1d681cba3e5929d5eab5fa82f91c1a
MariuszBotwina <mar.botwina@gmail.com>
2019-03-25 20:52:30 +0100

    cos co robilem

---
+++ src/users/MainController.java
@@ -0,0 +1,27 @@
+package users;
+
+import javafx.fxml.FXML;
+
+import java.awt.*;
+
+
+public class MainController {
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    private TextField txtUserName;
+
+
+    @FXML
+    private TextField txtPassword;
+    public void Login(ActiveEvent event){
+        if (txtUserName.getText().equals("User") && txtPassword.getText().equals("password")){
+            lblStatus.setText("Logina Success");
+        }
+        else {lblStatus.setText("Login Failed");
+    }
+
+
+
+}}
